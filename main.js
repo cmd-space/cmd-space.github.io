434c5fe93ecdbd1948621e4a5b790c86e66e1ca8
Mason Crane <masonecrane@gmail.com>
2022-05-28 21:52:21 -0600

    added date tracking and resetting every day

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+function getTheDate() {
+    let date = new Date();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let today = month + 1 + '-' + day + '-' + year;
+    return today;
+}
+
+function select(color) {
+    localStorage.setItem('selected', color)
+    localStorage.setItem('date', getTheDate())
+}
+
 window.onload = () => {
+    let today = getTheDate();
     let selected = localStorage.getItem('selected');
+    if (localStorage.getItem('date') != today) {
+        selected = 'green';
+    }
     document.getElementById(selected).classList.add('selected')
+    select(selected);
 }
 
 document.addEventListener('click', event => {
@@ -8,6 +27,6 @@ document.addEventListener('click', event => {
     if (event.target.tagName == 'DIV' && event.target.parentNode.className == 'main') {
         document.querySelectorAll('div').forEach(div => div.classList.remove('selected'));
         event.target.classList.add('selected');
-        localStorage.setItem('selected', event.target.id)
+        select(event.target.id);
     }
 });
